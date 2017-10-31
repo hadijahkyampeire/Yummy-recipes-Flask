43f458f9c4cd5e12b1bcb5db51f99f0386d7fb48
kyampeire Hadijah <30952856+hadijahkyampeire@users.noreply.github.com>
2017-10-31 07:37:13 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -4,9 +4,15 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return render_template('index.html')
-@app.route("/showSignUp")
-def showSignUp():
+@app.route("/showsignup")
+def showsignup():
     return render_template('signup.html')
+@app.route("/showlogin")
+def showlogin():
+	return render_template('login.html')
+@app.route('/viewrecipes')
+def viewrecipes():
+	return render_template('addrecipe.html')
 
 if __name__=="__main__":
     app.run(debug=True)
